0043bc6553d4b254543e02e1da204391cb59afea
bfourk <100809777+bfourk@users.noreply.github.com>
2024-02-23 23:56:38 +0000

    Update build-and-package.sh
    Script now makes a dedicated packages folder
    Script now removes the Release folder from bin
    Script now removes *.pdb files in publish folders

--- FileDiff/build-and-package.sh
+++ FileDiff/build-and-package.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 rm -rf bin/Release
+rm -rf packages
+mkdir packages
 dotnet publish -c Release -r linux-x64 --self-contained
 dotnet publish -c Release -r win-x64 --self-contained
 mv bin/Release/net6.0/linux-x64 bin/Release/net6.0/linux-x64-contained
@@ -8,16 +10,20 @@ dotnet publish -c Release -r linux-x64 --no-self-contained
 dotnet publish -c Release -r win-x64 --no-self-contained
 
 cd bin/Release/net6.0/linux-x64/publish
-7z a ../../FileDiff-linux64.7z * -mx9
+rm *.pdb
+7z a ../../../../../packages/FileDiff-linux64.7z * -mx9
 cd ../../
 cd win-x64/publish
-7z a ../../FileDiff-win64.7z * -mx9
+rm *.pdb
+7z a ../../../../../packages/FileDiff-win64.7z * -mx9
 cd ../../
 cd linux-x64-contained/publish
-7z a ../../FileDiff-linux64-contained.7z * -mx9
+rm *.pdb
+7z a ../../../../../packages/FileDiff-linux64-contained.7z * -mx9
 cd ../../
 cd win-x64-contained/publish
-7z a ../../FileDiff-win64-contained.7z * -mx9
-cd ../../
-rm -rf linux-x64 win-x64 win-x64-contained linux-x64-contained
+rm *.pdb
+7z a ../../../../../packages/FileDiff-win64-contained.7z * -mx9
+cd ../../../../..
+rm -rf bin/Release
 echo Complete
